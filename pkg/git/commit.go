fddbbdb6eca3548d8b6ede8420d3244451543a35
hau <ngtranhau@gmail.com>
2021-01-05 17:22:09 +0700

    feat(git): add hash in Commit

--- pkg/git/commit.go
+++ pkg/git/commit.go
@@ -3,11 +3,13 @@ package git
 import "github.com/go-git/go-git/v5/plumbing/object"
 
 type Commit struct {
+	Hash    string
 	Message string
 }
 
 func newCommit(commit *object.Commit) Commit {
 	return Commit{
+		Hash:    commit.Hash.String(),
 		Message: commit.Message,
 	}
 }
