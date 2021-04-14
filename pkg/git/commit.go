fffdd6514f683eda2ec59e51756b592a98e3d650
Tran Hau <ngtranhau@gmail.com>
2021-04-14 10:41:28 +0000

    feat(git): add author time

--- pkg/git/commit.go
+++ pkg/git/commit.go
@@ -1,15 +1,21 @@
 package git
 
-import "github.com/go-git/go-git/v5/plumbing/object"
+import (
+	"time"
+
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
 
 // Commit stores all git-commit information
 type Commit struct {
 	Message string
+	Time    time.Time
 }
 
 // Convert from git-commit
 func newCommit(commit *object.Commit) Commit {
 	return Commit{
 		Message: commit.Message,
+		Time:    commit.Author.When,
 	}
 }
